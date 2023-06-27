fbbbfa50f1be68c90f54811ef0b4137d3fc4d952
unknown <deividcuellocuevas@gmail.com>
2023-06-27 11:22:59 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Youtube clone using tailwind with dark mode
+![image](https://github.com/deividcuello/yt-clone-tailwind/assets/112868702/0badb990-974a-48f6-b1e6-c342e64df4ad)
+
+![image](https://github.com/deividcuello/yt-clone-tailwind/assets/112868702/74aca53a-7f02-4c81-8205-3602c6f19278)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./dist/*.{html,js}"],
+  darkMode: 'class',
   theme: {
     colors: {
       gray: '#D3D3D3',
       lightGray: '#F0F0F0',
       darkGray: '#606060',
+      darkGray1: '#707070',
       blue: '#3E6DD4',
       black: '#000000',
-      white: '#ffffff'
+      black1: '#222222',
+      white: '#ffffff',
+      red: '#ff0000'
     },
     extend: {
       fontFamily: {
